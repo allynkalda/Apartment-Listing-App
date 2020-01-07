c7bf768e5fd34aada88a53f00b361033c80cb112
Allyn Alda <allyn.alda@gmail.com>
2020-01-07 14:41:15 +0100

    test Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,9 +7,6 @@ export default function Navbar({ clickHandler }) {
 
   const [ toggleArrow, setToggleArrow ] = useState(false);
 
-  // <input type="text" placeholder="Barcelona"></input>
-  // <Button variant="contained" color="primary">Search</Button>
-
   return (
     <div className="navbar">
       <span>Filtros</span>
--- client/src/components/Navbar.test.js
+++ client/src/components/Navbar.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 import Navbar from './Navbar';
+import Button from '@material-ui/core/Button';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+const clickHandler = jest.fn();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -15,3 +20,16 @@ it('should render with class navbar', () => {
   const div = component.findByType('div');
   expect(div.props.className).toBe('navbar');
 });
+
+it('should render button and show correct icon onclick', () => {
+  const renderer = TestRenderer.create(<Navbar clickHandler={clickHandler} />);
+  const component = renderer.root;
+  const button = component.findByType(Button);
+  const arrowUp = component.findByType(ArrowDropUpIcon);
+  expect(button).toBeDefined();
+  expect(arrowUp).toBeDefined();
+  button.props.onClick();
+  const arrowDown = component.findByType(ArrowDropDownIcon);
+  expect(arrowDown).toBeDefined();
+});
+
